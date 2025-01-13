88d7903d77dcdbe585b5916220557b391debd712
HARSHIT BANSAL <harshit.bansal1236@gmail.com>
2025-01-13 22:20:30 +0530

    Password Hashing Changed

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -56,7 +56,7 @@ userSchema.pre('save', async function(next){
     if(!this.isModified('password')){
         next();
     }
-    this.password = await bcrypt.hash(this.password, 12);
+    this.password = await bcrypt.hash(this.password, 6);
     next();
 })
 
